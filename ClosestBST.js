fc4308585c4accf96910dce1a619a60f0a5bf169
William Watson <williampwatson1991@gmail.com>
2021-04-26 16:01:30 -0400

    feat: ClosestBST algo added, sudo code preplan

---
+++ ClosestBST.js
@@ -0,0 +1,7 @@
+//Write a function that takes in a binary search tree and a target integer value and returns the the closest value to that target value.
+
+//iterate through tree using search parameters
+//define helper function to keep track of closest node value
+//compare current value to following value and determine which value is closest to target
+//traverse tree based on whether or not the current value is less than or greater than target value (left, right)
+//edge cases possible null tree or node value === target value, in which case we can just return the target value.
