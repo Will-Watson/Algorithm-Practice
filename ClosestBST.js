e22cca9680ae562bc8639fd470524dc0806a0d31
William Watson <williampwatson1991@gmail.com>
2021-04-26 16:12:45 -0400

    feat: ClosestBST recursive solution added

--- ClosestBST.js
+++ ClosestBST.js
@@ -5,3 +5,45 @@
 //compare current value to following value and determine which value is closest to target
 //traverse tree based on whether or not the current value is less than or greater than target value (left, right)
 //edge cases possible null tree or node value === target value, in which case we can just return the target value.
+
+//BST CLASS
+
+/*
+
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+*/
+
+//recursive solution
+
+const findClosestValueBST = (tree, value) => {
+  const findClosestHelper = (tree, target, closest) => {
+    if (tree === null) {
+      return closest;
+    }
+
+    if (tree.value === target) {
+      return target;
+    }
+
+    if (Math.abs(target - closest) > Math.abs(target - tree.value)) {
+      closest = tree.value;
+    }
+
+    if (target < tree.value) {
+      return findClosestHelper(tree.left, target, closest);
+    } else {
+      return findClosestHelper(tree.right, target, closest);
+    }
+
+    return closest;
+  };
+
+  return findClosestHelper(tree, target, (closest = Infinity));
+};
