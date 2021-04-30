a6b7a5e33cca2cb0722ecabede474f4bad3840a0
William Watson <williampwatson1991@gmail.com>
2021-04-30 16:38:47 -0400

    feat: implemented naive solution bubble sort

--- Sorts.js
+++ Sorts.js
@@ -17,3 +17,27 @@ const ES6swap = (arr, idx1, idx2) => {
 };
 
 //bubble sort
+/*
+
+-start looping from the end of the array towards the begining with a vaiable, i. Backwards
+-start an inner loop with a variable called j from the beginning until i - 1
+-if arr[j] is greater than arr[j + 1], swap those two values
+-return sorted array
+
+*/
+
+//naive solution
+
+const bubbleSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] < arr[j + 1]) {
+        //swap
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+};
