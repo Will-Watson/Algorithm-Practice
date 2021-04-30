94a775e0e680269bab7211ff935eddaa4ba4ff78
William Watson <williampwatson1991@gmail.com>
2021-04-30 14:10:35 -0400

    feat: refactored code

--- BinarySearch.js
+++ BinarySearch.js
@@ -19,3 +19,16 @@ const binarySearch = (arr, val) => {
     return false;
   }
 };
+
+const refactoredBinarySearch = (arr, val) => {
+  let start = 0;
+  let end = arr.length - 1;
+  let mid = Math.floor((start + end) / 2);
+
+  while (arr[mid] !== val && start <= end) {
+    if (val < arr[mid]) end = mid - 1;
+    else start = mid + 1;
+    mid = Math.floor((start + end) / 2);
+  }
+  return arr[mid] === val ? mid : false;
+};
