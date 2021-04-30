fb11a01fd6a18f7b37d212a4977c0178181e22ac
William Watson <williampwatson1991@gmail.com>
2021-04-30 14:07:11 -0400

    feat: basica binary search on sorted array

---
+++ BinarySearch.js
@@ -0,0 +1,21 @@
+//given a sorted array and a value, implement a binary search to return the index of the value or return false.
+
+const binarySearch = (arr, val) => {
+  let start = 0;
+  let end = arr.length - 1;
+  let mid = Math.floor((start + end) / 2); // average, or, middle of the array.
+
+  while (arr[mid] !== val && start <= end) {
+    if (val < arr[mid]) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+    mid = Math.floor((start + end) / 2);
+  }
+  if (arr[mid] === val) {
+    return mid;
+  } else {
+    return false;
+  }
+};
