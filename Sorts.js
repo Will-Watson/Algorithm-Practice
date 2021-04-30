3154f9f39f2d04dee22dd8d7c878965b10495635
William Watson <williampwatson1991@gmail.com>
2021-04-30 17:28:54 -0400

    feat: selectionSort added

--- Sorts.js
+++ Sorts.js
@@ -63,3 +63,46 @@ const _bubbleSort = (arr) => {
 };
 
 //noSwaps is checking to see if the arr is already sorted and stopping the code if it is in order to shorten run time, time complexity.
+
+//selection sort
+
+//selection sort, like bubble sort compares two values but first loops through to find the minimum value then switches that value to the front. Incrementing the starting point as we go, finding the smallest value and switching it to the front
+
+//this algo is great for data thats almost sorted already
+//super slow otherwise
+
+/*
+
+[5, 3, 4, 1, 2]
+ ^        ^
+[1, 3, 4, 5, 2]
+    ^        ^
+[1, 2, 4, 5, 3]
+
+etc...
+
+-store the first element as the smallest value you've seen so far.
+-compare this item to the next item in the array until you find a smaller number
+-if a smaller number is found, designate that smaller number to be the new "minimum" and continue until the end of the array.
+-if the 'minimum' is not the value (index) you initially began with, swap the two values
+-repeat this with the next element until the array is sorted.
+
+*/
+
+const selectionSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowest]) {
+        lowest = j;
+      }
+    }
+    //swap
+    if (i !== lowest) {
+      let temp = arr[i];
+      arr[i] = arr[lowest];
+      arr[lowest] = temp;
+    }
+  }
+  return arr;
+};
