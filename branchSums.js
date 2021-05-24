e53033fc74c1d70ba055e4598d621619880d8ecb
William Watson <williampwatson1991@gmail.com>
2021-05-24 18:53:32 -0400

    feat: added sudo code, approach and strategy to branchSums BST algo

--- branchSums.js
+++ branchSums.js
@@ -11,3 +11,9 @@ given a BST, return an array of the sum of all branches
 result = [7, 7, 10]
 
 */
+
+//initialize empty array to store each sum
+//traverse each branch of the tree while adding each node value to a place holder variable
+//once leaf is reached, push placeholder variable value into initialized array
+//recursively repeat steps for each branch until entire tree has been traversed
+//important to remember that the length of the array will be the exact same number as the number of branches. Thus making this a O(n) time and space complexity
