6c76c71886f4f5c9cf3c1322687ecdf857fb3bcf
William Watson <williampwatson1991@gmail.com>
2021-05-24 18:54:23 -0400

    feat: added initial code and helper function solution

--- branchSums.js
+++ branchSums.js
@@ -17,3 +17,25 @@ result = [7, 7, 10]
 //once leaf is reached, push placeholder variable value into initialized array
 //recursively repeat steps for each branch until entire tree has been traversed
 //important to remember that the length of the array will be the exact same number as the number of branches. Thus making this a O(n) time and space complexity
+
+function branchSums(root) {
+  const result = [];
+  branchSumsHelper(root, 0, result);
+  return result;
+}
+
+function branchSumsHelper(root, count, result) {
+  if (!root) {
+    return;
+  }
+
+  let newCount = count + root.value;
+
+  if (root.left === null && root.right === null) {
+    result.push(newCount);
+    return;
+  }
+
+  branchSumsHelper(root.left, newCount, result);
+  branchSumsHelper(root.right, newCount, result);
+}
