b08600865da4711067c5737bdb5bbf84f73d5279
William Watson <williampwatson1991@gmail.com>
2021-06-08 12:37:47 -0400

    feat: push added to doubly linked list crash course

--- DoublyLL.js
+++ DoublyLL.js
@@ -14,4 +14,19 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val) {
+    let newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
