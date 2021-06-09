63c1b3a1dbd76bcff5609af4d9eab69cd5af214f
William Watson <williampwatson1991@gmail.com>
2021-06-09 13:58:29 -0400

    Big O notation time complexities added to bottom of file based on method

--- DoublyLL.js
+++ DoublyLL.js
@@ -140,3 +140,14 @@ class DoublyLinkedList {
     return removedNode;
   }
 }
+
+/*
+
+BIG O NOTATION OF DOUBLY LINKED LIST
+
+Insertion - O(1)
+Removal - O(1)
+Searching - O(n)
+Access - O(n)
+
+*/
