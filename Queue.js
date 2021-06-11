58a14e9eff7e2d120833373a01f5ce62a9b5ac7d
William Watson <williampwatson1991@gmail.com>
2021-06-11 16:18:40 -0400

    feat: queue crash course file created. explanation notes added

---
+++ Queue.js
@@ -0,0 +1,7 @@
+//Queue crash course
+//Similar to a stack in that its an abstract data structure with two functions, adding and removing
+//however instead of LIFO, a Queue is a FIFO. First in First out.
+
+//good for background tasks
+//uploading resources
+//printing/task processing
