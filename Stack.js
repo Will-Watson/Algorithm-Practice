0dd1f265c490566b7187e58d0400a245d68434fc
William Watson <williampwatson1991@gmail.com>
2021-06-11 15:44:35 -0400

    feat: stack crash course file added, exmaples provided

---
+++ Stack.js
@@ -0,0 +1,23 @@
+//stack crash course
+//abstract data structure
+//collection of data that needs to abide by a LIFO principle which is last in first out
+
+const stack = [];
+
+stack.push('google');
+stack.push('instagram');
+stack.push('youtube');
+
+stack.pop();
+
+//example of a LIFO stack, push and pop in tandum
+
+const stack2 = [];
+
+stack.unshift('create new file');
+stack.unshift('reszied file');
+stack.unshift('cloned out wrinkle');
+
+stack.unshift();
+
+//another example.. but obviously push and pop is better for big O notation as its faster
