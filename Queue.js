ebbfb8e90ec3c633abcec33f969313c04acc05a4
William Watson <williampwatson1991@gmail.com>
2021-06-11 16:19:56 -0400

    feat: Queue class added to queue crash course

--- Queue.js
+++ Queue.js
@@ -5,3 +5,18 @@
 //good for background tasks
 //uploading resources
 //printing/task processing
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+}
