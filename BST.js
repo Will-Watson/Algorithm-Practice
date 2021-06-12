1997b7e9a75f9853f23ba4f85eb0f15f33a8081e
William Watson <williampwatson1991@gmail.com>
2021-06-12 12:50:40 -0400

    feat: BST crash course review file added. Some BST terminology

---
+++ BST.js
@@ -0,0 +1,7 @@
+//Binary Search Tree crash course and review
+//root is the top node in a tree
+//child is a node directly connected to another node when moving away from the root
+//parent, is the converse notion of a child
+//siblings are groups of nodes with the same parent
+//a leaf is a node with no children but also a child of another node
+//edge, the connection between two nodes
