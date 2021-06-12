1c172f997bdb36e96fdfb51f1e3317d8d745ebc8
William Watson <williampwatson1991@gmail.com>
2021-06-12 14:31:53 -0400

    feat: DFSInOrder method added to BST review

--- BST.js
+++ BST.js
@@ -102,6 +102,18 @@ class BST {
     traverse(this.root);
     return data;
   }
+  DFSInOrder() {
+    let data = [];
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      //node.left && traverse(node.left);
+      data.push(node.val);
+      if (node.right) traverse(node.right);
+      //node.right && traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
+  }
 }
 
 let tree = new BST();
