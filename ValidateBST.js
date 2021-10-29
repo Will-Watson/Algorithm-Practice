4c63d873cd8e259c5f2f6963976f7e4f8d75b69a
William Watson <williampwatson1991@gmail.com>
2021-10-29 17:06:45 -0400

    feat: added validateBST solution

--- ValidateBST.js
+++ ValidateBST.js
@@ -14,3 +14,17 @@ class BST {
     this.right = null;
   }
 }
+
+function validateBst(tree) {
+  return validateBstHelper(tree, -Infinity, Infinity);
+}
+
+function validateBstHelper(tree, minValue, maxValue) {
+  if (!tree) return true;
+
+  if (tree.value < minValue || tree.value >= maxValue) return false;
+
+  const leftIsValid = validateBstHelper(tree.left, minValue, tree.value);
+
+  return leftIsValid && validateBstHelper(tree.right, tree.value, maxValue);
+}
