00935d0a5a8e927961ab6501b0b1352f1c08f44f
William Watson <williampwatson1991@gmail.com>
2021-10-29 17:05:51 -0400

    feat: added validateBST query

---
+++ ValidateBST.js
@@ -0,0 +1,8 @@
+/*
+Write a function that takes in a potentially invalid Binary Search Tree (BST) and returns a boolean representing whether the BST is valid.
+
+each BST node has an integer value, a left child noe, and a right child node. A node is said to be valid BST node if and only if it satisfies the BST property: it's value is strictly greater than the values of every node to its left; its value is less than or equal to the values of every node to its right; and its children noes are either valid BST nodes themselves or None / null.
+
+A BST is valid if and only if all of its noes are valid BST nodes.
+
+*/
