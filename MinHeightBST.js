3f7e4c7c9e78999c6ff1f79824856f36055d5ecb
William Watson <williampwatson1991@gmail.com>
2021-10-29 16:58:55 -0400

    feat: added first solution to min height

--- MinHeightBST.js
+++ MinHeightBST.js
@@ -34,3 +34,23 @@ class BST {
     }
   }
 }
+
+function minHeightBst(array) {
+  return constructMinHeightBst(array, null, 0, array.length - 1);
+}
+
+function constructMinHeightBst(array, bst, startIdx, endIdx) {
+  if (endIdx < startIdx) return;
+
+  const midIdx = Math.floor((startIdx + endIdx) / 2);
+  const valueToAdd = array[midIdx];
+
+  if (!bst) {
+    bst = new BST(valueToAdd);
+  } else {
+    bst.insert(valueToAdd);
+  }
+  constructMinHeightBst(array, bst, startIdx, midIdx - 1);
+  constructMinHeightBst(array, bst, midIdx + 1, endIdx);
+  return bst;
+}
