80a5c002181b88000bac53825ed2bbf9b246d5d0
William Watson <williampwatson1991@gmail.com>
2021-11-01 16:40:25 -0400

    feat: added solution 2

--- ReconstructBST.js
+++ ReconstructBST.js
@@ -50,3 +50,42 @@ class TreeInfo {
     this.rootIdx = rootIdx;
   }
 }
+
+function reconstructBst2(preOrderTraversalValues) {
+  const treeInfo = new TreeInfo(0);
+  return reconstructBstFromRange(
+    -Infinity,
+    Infinity,
+    preOrderTraversalValues,
+    treeInfo
+  );
+}
+
+function reconstructBstFromRange(
+  lowerBound,
+  upperBound,
+  preOrderTraversalValues,
+  currentSubtreeInfo
+) {
+  if (currentSubtreeInfo.rootIdx === preOrderTraversalValues.length)
+    return null;
+
+  const rootValue = preOrderTraversalValues[currentSubtreeInfo.rootIdx];
+  if (rootValue < lowerBound || rootValue >= upperBound) return null;
+
+  currentSubtreeInfo.rootIdx += 1;
+  const leftSubtree = reconstructBstFromRange(
+    lowerBound,
+    rootValue,
+    preOrderTraversalValues,
+    currentSubtreeInfo
+  );
+  const rightSubtree = reconstructBstFromRange(
+    rootValue,
+    upperBound,
+    preOrderTraversalValues,
+    currentSubtreeInfo
+  );
+
+  return new BST(rootValue, leftSubtree, rightSubtree);
+}
