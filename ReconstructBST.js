b7679734e359cb231211b4722874849f077257ee
William Watson <williampwatson1991@gmail.com>
2021-11-01 16:39:03 -0400

    feat: added solution one

--- ReconstructBST.js
+++ ReconstructBST.js
@@ -21,3 +21,26 @@ class BST {
     this.right = right;
   }
 }
+
+function reconstructBst(preOrderTraversalValues) {
+  if (preOrderTraversalValues.length === 0) return null;
+
+  const currentValue = preOrderTraversalValues[0];
+  let rightSubtreeRootIdx = preOrderTraversalValues.length;
+
+  for (let i = 1; i < preOrderTraversalValues.length; i++) {
+    if (preOrderTraversalValues[i] >= currentValue) {
+      rightSubtreeRootIdx = i;
+      break;
+    }
+  }
+
+  const leftSubtree = reconstructBst(
+    preOrderTraversalValues.slice(1, rightSubtreeRootIdx)
+  );
+  const rightSubtree = reconstructBst(
+    preOrderTraversalValues.slice(rightSubtreeRootIdx)
+  );
+
+  return new BST(currentValue, leftSubtree, rightSubtree);
+}
