43dbb27fd60b6411d1e29968bc48bca3d950984e
William Watson <williampwatson1991@gmail.com>
2021-11-01 16:38:10 -0400

    feat: added reconstructBST prompt

---
+++ ReconstructBST.js
@@ -0,0 +1,15 @@
+/*
+
+The pre-order traversal of a Binary Tree is a traversal technique that starts at the tree's root node and visits nodes in the following order:
+
+1. Current node.
+2. Left subtree.
+3. Right subtree.
+
+Given a non-empty array of integeres representing the pre=order traversal of a binary Search Tree (BST), write a function that creates the relevant BST and returns its root node.
+
+The inpur array will contain the values of BST nodes in the order in which these nodes would be visited with a pre-order traversal.
+
+Each BST node has an integer value, a left child noe, and a right child node. A node is said to be valid BST node if and only if it satisfies the BST property: it's value is strictly greater than the values of every node to its left; its value is less than or equal to the values of every node to its right; and its children noes are either valid BST nodes themselves or None / null.
+
+*/
