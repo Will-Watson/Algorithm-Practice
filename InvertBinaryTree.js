da7703dee3f721b0fba1b4e65bbe4fd5d1cce0d6
William Watson <williampwatson1991@gmail.com>
2021-11-02 11:11:21 -0400

    feat: added BinaryTree class to file

--- InvertBinaryTree.js
+++ InvertBinaryTree.js
@@ -5,3 +5,11 @@ Write a function that takes in a Binary Tree and inverts it. In other words, the
 Each BinaryTree node has an integer value, a left child node, and a rigth child node. Children nodes can either be BinaryTree nodes themselves or None/ null.
 
 */
+
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
