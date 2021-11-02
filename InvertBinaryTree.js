cedb1fe2b2c39b3e27231ec72e0a027384596a99
William Watson <williampwatson1991@gmail.com>
2021-11-02 11:11:58 -0400

    feat: added my rockstar recursive solution, solution one

--- InvertBinaryTree.js
+++ InvertBinaryTree.js
@@ -13,3 +13,23 @@ class BinaryTree {
     this.right = null;
   }
 }
+
+//recursive solution
+
+function invertBinaryTree(tree) {
+  let temp = tree.left;
+  tree.left = tree.right;
+  tree.right = temp;
+
+  if (tree.left) {
+    invertBinaryTree(tree.left);
+  }
+
+  if (tree.right) {
+    invertBinaryTree(tree.right);
+  }
+
+  if (!tree.right && !tree.left) {
+    return null;
+  }
+}
