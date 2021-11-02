ddfcdce6cbbafd975b66d6820fd2bd630b737325
William Watson <williampwatson1991@gmail.com>
2021-11-02 11:10:57 -0400

    feat: added algo prompt to file

---
+++ InvertBinaryTree.js
@@ -0,0 +1,7 @@
+/*
+
+Write a function that takes in a Binary Tree and inverts it. In other words, the function should swap every left node in the tree for its corresponding right node.
+
+Each BinaryTree node has an integer value, a left child node, and a rigth child node. Children nodes can either be BinaryTree nodes themselves or None/ null.
+
+*/
