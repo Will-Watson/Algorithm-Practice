72693d3304083bef705c8afa8998019033b5ed9a
William Watson <williampwatson1991@gmail.com>
2021-11-08 15:27:49 -0500

    feat: added FindSuccessor prompt

---
+++ FindSuccessor.js
@@ -0,0 +1,11 @@
+/*
+
+Write a function that takes in a Binary Tree (where nodes have an additional pointer to their parent node) as well as a node contained in that tree and returns the given node's successor.
+
+A node's successor is the next node to be visited (immediately after the given node) when treversint ites tree using the in-order tree-traversal technique. A node has no succesor if its the last node to be visited in the in-order traversal.
+
+if a node has no successor, your function should return None/null.
+
+Each Binary Tree nodes has an integer value, a parent node, a left child node, and a rigth child node. Children nodes can either be Binary Tree nodes themselves or None/null.
+
+*/
