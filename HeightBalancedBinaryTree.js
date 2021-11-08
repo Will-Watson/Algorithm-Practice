a1bcf05b0e5fda34d11ce7d7ff6b045095322dbf
William Watson <williampwatson1991@gmail.com>
2021-11-08 17:19:27 -0500

    feat: added HeightBalancedBinaryTree prompt

---
+++ HeightBalancedBinaryTree.js
@@ -0,0 +1,9 @@
+/*
+
+You're given the root node of a Binary Tree. Write a function that returns true if this Binary Tree is Height Balanced and Flase if it isn't.
+
+A Binary Tree is height balanced if for each node in the tre, the difference between the height of its left subtree and the height of its right subtree is at most 1.
+
+Each BinaryTree node has an interger value, a left child node, and a right child node. Children nodes can either be BinaryTree nodes themselves or None/null.
+
+*/
