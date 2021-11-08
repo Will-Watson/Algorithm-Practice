6cd37cde77841cba4ccf79c85cb8cf2370fa291d
William Watson <williampwatson1991@gmail.com>
2021-11-08 17:29:39 -0500

    feat: getTreeInfo helper function added

--- HeightBalancedBinaryTree.js
+++ HeightBalancedBinaryTree.js
@@ -22,3 +22,19 @@ class TreeInfo {
     this.height = height;
   }
 }
+
+function getTreeInfo(node) {
+  if (!node) return new TreeInfo(true, -1);
+
+  const leftSubtreeInfo = getTreeInfo(node.left);
+  const rightSubtreeInfo = getTreeInfo(node.right);
+
+  const isBalanced =
+    leftSubtreeInfo.isBalanced &&
+    rightSubtreeInfo.isBalanced &&
+    Math.abs(leftSubtreeInfo.height - rightSubtreeInfo.height) <= 1;
+
+  const height = Math.max(leftSubtreeInfo.height, rightSubtreeInfo.height) + 1;
+
+  return new TreeInfo(isBalanced, height);
+}
