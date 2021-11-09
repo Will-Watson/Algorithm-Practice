e0147e06a33fa7d398a31f1b91bc2874cbd3b92e
William Watson <williampwatson1991@gmail.com>
2021-11-09 12:27:42 -0500

    feat: added second solution to file

--- MaxSubsetSum.js
+++ MaxSubsetSum.js
@@ -19,3 +19,19 @@ function maxSubsetSumNoAdjacent(array) {
   }
   return maxSums[maxSums.length - 1];
 }
+
+function maxSubsetSumNoAdjacent2(array) {
+  if (!array.length) return 0;
+
+  if (array.length === 1) return array[0];
+
+  let second = array[0];
+  let first = Math.max(array[0], array[1]);
+
+  for (let i = 2; i < array.length; i++) {
+    let current = Math.max(first, second + array[i]);
+    second = first;
+    first = current;
+  }
+  return first;
+}
