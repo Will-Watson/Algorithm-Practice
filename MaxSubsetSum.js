08870b99d82c51e90a668beb52927d58ea183d07
William Watson <williampwatson1991@gmail.com>
2021-11-09 12:26:42 -0500

    feat: MaxSubsetSum prompt added to file

---
+++ MaxSubsetSum.js
@@ -0,0 +1,7 @@
+/*
+
+Write a function that takes in an array of positive integers and returns the maximum sum of non-adjacent elements in the array.
+
+If the inpur array is empty, the function should return 0
+
+*/
