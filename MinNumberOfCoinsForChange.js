377d0238c170401393089d66b8bff69619ea20b2
William Watson <williampwatson1991@gmail.com>
2021-11-09 13:11:22 -0500

    feat: prompt added to MinNumberOfCoinsForChange algo

---
+++ MinNumberOfCoinsForChange.js
@@ -0,0 +1,9 @@
+/*
+
+Given an array of positive integers representing coin denominations and a single non-negative integer n representing a target amount of money, write a function that returns the smallest number of coins needed to make change for (to sum up to) that target amount using the given coin denominations.
+
+Note that you have access to an unlimited amount of coines. in other words, if the dinominations are [1, 5, 10], you have access to an unlimited amount of 1's, 5's, 10's.
+
+If it's impossible to make change for the target amount, return -1.
+
+*/
